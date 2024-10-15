2ca6b4f98c70d70f81f2fc66d390326d08353719
Andreas Karz <andreas.karz@outlook.com>
2024-10-15 12:36:28 +0200

    Init 2

--- SnapshooterUnitTest.cs
+++ SnapshooterUnitTest.cs
@@ -1,4 +1,5 @@
 using Snapshooter.Xunit;
+using System.Diagnostics;
 using System.Text.Json;
 
 namespace Snapshooter_Tests
@@ -7,29 +8,35 @@ namespace Snapshooter_Tests
     {
 
         [Fact]
-        public void ObjectMatch()
+        public void Assert_AssertEqualObj_AssertSuccessful()
         {
             var MyStudent = new Student(1, "abc"); 
             Snapshot.Match(MyStudent);
         }
 
         [Fact] 
-        public void Todos()
+        public void Assert_AssertEqualJson_AssertSuccessfulInTime()
         {
-            var todoJson = GetUser();
-            Snapshot.Match(todoJson);
+            var (userJsonString, timeTaken) = GetUserWithTiming();
+            Snapshot.Match(userJsonString);
+            Assert.True(timeTaken < 1000);
         }
 
-        private static string GetUser()
+
+        
+        private static (string, long) GetUserWithTiming()
         {
             var url = "https://jsonplaceholder.typicode.com/users/2";
 
             using HttpClient client = new();
+            Stopwatch stopwatch = Stopwatch.StartNew();
             HttpResponseMessage response = client.GetAsync(url).Result;
             response.EnsureSuccessStatusCode();
             string responseBody = response.Content.ReadAsStringAsync().Result;
             JsonDocument jsonDoc = JsonDocument.Parse(responseBody);
-            return responseBody;
+            stopwatch.Stop();
+            long elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+            return (responseBody, elapsedMilliseconds);
         }
     }
 
